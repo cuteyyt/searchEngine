cfa41eb8b4075984aaad09c24361050782551b2e
Todobe <todobe@qq.com>
2021-06-26 19:25:38 +0800

    add output format

--- miniSearchEngine/search_engine/bool_search.py
+++ miniSearchEngine/search_engine/bool_search.py
@@ -1,5 +1,6 @@
 from .spell_correction import correct_bad_words
 from .wildcards_search import get_wildcards_word
+from .output_format import warning_info, error_info, plain_info, highlight_info
 
 
 BRACKETS_LEFT = "("
@@ -10,6 +11,15 @@ OPT_OR = "|"
 OPT_NOT = "~"
 WILDCARDS_STAR = "*"
 
+
+def spell_correction_info(word, new_word):
+    highlight_info(word + " can't be recognized. Do you mean '" + new_word + "' ?")
+
+    highlight_info("We have shown you the result of '" + new_word + "'")
+
+    plain_info("If you still want to search '" + word + "', please use such command to close word correction:")
+
+    warning_info('"! close word correction" or "! close wc"')
 
 def bool_opt_and(docs1, docs2):
     ret = []
@@ -56,7 +66,7 @@ def bool_opt_not(docs1, max_doc_num=100):
     ret = []
     len1 = len(docs1)
     index = 0
-    for doc_id in range(1,max_doc_num):
+    for doc_id in range(1, max_doc_num):
         if index < len1 and doc_id == docs1[index]:
             index += 1
         else:
@@ -66,7 +76,7 @@ def bool_opt_not(docs1, max_doc_num=100):
 
 def bool_search(query_list, term_dict, word_correction=True, wildcards_search=True):
     if len(query_list) == 0:
-        print("Input can't be empty.")
+        error_info("Input can't be empty.")
         return
 
     query_list = query_list.split(' ')
@@ -81,7 +91,7 @@ def bool_search(query_list, term_dict, word_correction=True, wildcards_search=Tr
             sta.append(BRACKETS_LEFT)
         elif word == BRACKETS_RIGHT:
             if len(sta) < 2 or not isinstance(sta[-1], list) or sta[-2] != BRACKETS_LEFT:
-                print("ERROR! Syntax failed!")
+                error_info("ERROR! Syntax failed!")
                 return
             sta.pop(-2)
         elif word == OPT_AND:
@@ -95,26 +105,24 @@ def bool_search(query_list, term_dict, word_correction=True, wildcards_search=Tr
             if wildcards_search and WILDCARDS_STAR in word:
                 candidate_words = get_wildcards_word(word)
                 if candidate_words is None:
-                    print("wildcards can't be '*' only!")
+                    warning_info("wildcards can't be '*' only!")
                     return
 
                 term_slice = []
                 for candidate_word in candidate_words:
                     term_slice = bool_opt_or(term_slice, list(term_dict[candidate_word]['posting_list'].keys()))
                 sta.append(term_slice)
-                print("using wildcards search, words matched:", candidate_words)
-                print("docs with these words are:", term_slice)
+                highlight_info("using wildcards search, words matched: " + str(candidate_words))
+                plain_info("docs with these words are: " + str(term_slice))
             # no wildcards search
             else:
                 # word correction
                 if word_correction and word not in term_dict:
                     new_word = correct_bad_words(word)
                     if new_word != "":
-                        print(word, "can't be recognized. Do you mean", new_word, "?")
-                        print("We have show you the result of", new_word)
-                        print("If you still want to search", word, "please use ",
-                              '"! close word correction" or "! close wc" to close word correction')
+                        spell_correction_info(word, new_word)
                         word = new_word
+
                 # normal search
                 if word in term_dict:
                     term_slice = list(term_dict[word]['posting_list'].keys())
@@ -122,7 +130,7 @@ def bool_search(query_list, term_dict, word_correction=True, wildcards_search=Tr
                     term_slice = []
 
                 sta.append(term_slice)
-                print(word, term_slice)
+                plain_info(word + " " + str(term_slice))
 
         while isinstance(sta[-1], list):
             if len(sta) >= 2 and isinstance(sta[-2], list):
@@ -143,6 +151,6 @@ def bool_search(query_list, term_dict, word_correction=True, wildcards_search=Tr
                 break
 
     if len(sta) > 1 or not isinstance(sta[0], list):
-        print("ERROR! Syntax failed!")
+        error_info("ERROR! Syntax failed!")
         return
     return sta[0]
--- miniSearchEngine/search_engine/interaction.py
+++ miniSearchEngine/search_engine/interaction.py
@@ -1,5 +1,7 @@
 from .bool_search import bool_search
 from .pretreatment import initialize
+from .output_format import success_info, error_info, warning_info, plain_info
+
 
 close_word_correction = ["! close word correction", "! close wc"]
 open_word_correction = ["! open word correction", "! open wc"]
@@ -7,31 +9,29 @@ EXIT_COMMAND = ["! exit"]
 word_correction = True
 
 
-
 def start(term_dict=None, term_dict_with_positional_index=None, vector_model=None):
-    initialize(term_dict,term_dict_with_positional_index)
+    initialize(term_dict, term_dict_with_positional_index)
 
     global word_correction
     while True:
         query = input("come on, baby: ")
         if query in close_word_correction:
             word_correction = False
-            print("Close word correction.")
+            success_info("Close word correction.")
             continue
         if query in open_word_correction:
             word_correction = True
-            print("Open word correction.")
+            success_info("Open word correction.")
             continue
         if query in EXIT_COMMAND:
             break
         ret = bool_search(query, term_dict, word_correction)
         if ret is None:
-            print("Please input your query in format!")
+            warning_info("Please input your query in format!")
         else:
-            print(ret)
+            plain_info(ret)
 
-    print("Bye~")
-
+    success_info("Bye~")
 
 
 if __name__ == '__main__':
